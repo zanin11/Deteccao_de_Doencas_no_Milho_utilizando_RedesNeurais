816fe285e750a5a14a81a0bf45968eac72a64455
Guilherme Zanin <guilherme.zaninn13@gmail.com>
2024-11-26 15:12:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,13 @@ option = st.sidebar.selectbox(
 
 # Aba de Informações Gerais
 if option == "Informações Gerais":
+    st.header("Informações Acadêmicas")
+    st.write("""
+        Sistema desenvolvido como método avaliativo da matéria de Inteligência Artificial
+        Guilherme Zanin - RA: 221026479.
+        Ciência da Computação - Unesp/Bauru.
+        Novembro de 2024.
+    """)
     st.header("Sobre o Sistema")
     st.write("""
         Este sistema utiliza redes neurais para detectar doenças em folhas de milho. 
@@ -51,6 +58,20 @@ if option == "Informações Gerais":
         - **Healthy**: Folha saudável sem sinais de doenças.
         
         Carregue uma imagem de uma folha de milho e o sistema retornará a classificação da condição da folha.
+    """)
+
+    st.header("Sobre o Modelo Utilizado")
+    st.write("""
+        O modelo utilizado é baseado na arquitetura **MobileNetV2**, que foi treinado para identificar doenças nas folhas de milho. 
+        A MobileNetV2 foi escolhida por ser uma rede neural eficiente para dispositivos móveis e para aplicações que exigem uma boa precisão, mas com um modelo leve. 
+        
+        As camadas principais do modelo incluem:
+        - **Base do modelo MobileNetV2**: A base do modelo é carregada com pesos pré-treinados do ImageNet.
+        - **GlobalAveragePooling2D**: Camada de pooling para reduzir as dimensões dos dados.
+        - **Camada densa de 128 neurônios com ReLU**: Camada densa intermediária para capturar padrões complexos.
+        - **Camada de saída com 4 neurônios e função de ativação softmax**: Para classificar as folhas nas 4 classes possíveis (Blight, Common Rust, Gray Leaf Spot, Healthy).
+        
+        O modelo foi treinado por **10 épocas** para realizar a tarefa de classificação de imagens.
     """)
 
 # Aba de Classificação de Imagem
